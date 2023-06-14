29e1dae053e5daba360544a8707ef9c45568b681
Brian Rono <chikoe26@gmail.com>
2023-06-14 12:45:03 +0300

    styling input and button in signup.vue

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,6 +1,12 @@
 <template>
     <img class="logo" src="../assets/resto-logo.jpg"/>
-    <h1>Sign Up.</h1>
+    <h1>Sign Up</h1>
+    <div class="register">
+        <input type="text" placeholder="Enter Your Name" />
+        <input type="text" placeholder="Enter Your Email" />
+        <input type="password" placeholder="Enter Your Password" />
+        <button class="signupbtn">Sing Up</button>
+    </div>
 </template>
 <script>
 export default {
@@ -11,5 +17,29 @@ export default {
 <style>
 .logo {
     width:300px
+}
+
+.register input {
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid skyblue;
+}
+
+.signupbtn{
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    background: skyblue;
+    color: white;
+    border: 1px solid skyblue;
 }
 </style>
