f5448b45b17317fcb0ab99c49beba36d6fd1b40d
Brian Rono <chikoe26@gmail.com>
2023-06-14 12:29:30 +0300

    adding restaurant logo and styling it

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+
   <SignUp />
 </template>
 
 <script>
 
-import SignUp from ' ./components/SignUp.vue'
+import SignUp from "./components/SignUp.vue";
 export default {
   name: 'App',
   components :{
---
+++ src/components/SignUp.vue
@@ -0,0 +1,15 @@
+<template>
+    <img class="logo" src="../assets/resto-logo.jpg"/>
+    <h1>Sign Up.</h1>
+</template>
+<script>
+export default {
+    name :'SignUp'
+}
+</script>
+
+<style>
+.logo {
+    width:300px
+}
+</style>
