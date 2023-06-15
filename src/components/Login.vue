96a93be135d0382e28bf45814be2600fd35ec1dd
Brian Rono <chikoe26@gmail.com>
2023-06-15 12:52:49 +0300

    little edit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,11 @@
 <template>
     <img class="logo" src="../assets/resto-logo.jpg"/>
     <h1>Login</h1>
-    <div class="register">
+    <div class="login">
         <input type="text" v-model="email" placeholder="Enter Your Email" />
         <input type="password" v-model="password" placeholder="Enter Your Password" />
-        <button v-on:click="signup" class="signupbtn">Login</button>
-        <p><router-link to="/sign-up">Sign Up</router-link></p>
+        <button class="signupbtn">Login</button>
+        <p class="button"><router-link to="/sign-up">Sign Up</router-link></p>
     </div>
 </template>
 
