83d514cb7b67c962723cf6119d1367f4c20fca15
Brian Rono <chikoe26@gmail.com>
2023-06-15 13:01:38 +0300

    add click function to login button

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,13 +4,26 @@
     <div class="login">
         <input type="text" v-model="email" placeholder="Enter Your Email" />
         <input type="password" v-model="password" placeholder="Enter Your Password" />
-        <button class="signupbtn">Login</button>
+        <button class="signupbtn" v-on:click="Login">Login</button>
         <p class="button"><router-link to="/sign-up">Sign Up</router-link></p>
     </div>
 </template>
 
 <script>
 export default {
-    name:'LogIn'
-}
+    name:'LogIn',
+    data()
+    {
+        return{
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        Login()
+        {
+            console.warn(this.email,this.password)
+        }
+    }
+};
 </script>
