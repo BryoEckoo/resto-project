9c4737525538bde7ddc4f4edef3e47a35d066c58
Brian Rono <chikoe26@gmail.com>
2023-06-16 13:27:42 +0300

    m

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="nav">
-        <router-link to="/HomePage">Home</router-link>
+        <router-link to="/">Home</router-link>
         <router-link to="/add">Add Restaurant</router-link>
         <router-link to="/update">Update Restaurant</router-link>
         <a href="#" v-on:click="logout">Logout</a>
