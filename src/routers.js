0cbcea678c219e677223a68d8228fef87c90bf42
Brian Rono <chikoe26@gmail.com>
2023-06-16 13:09:45 +0300

    adding to routes

--- src/routers.js
+++ src/routers.js
@@ -1,13 +1,15 @@
-import HomePage from "./components/Home.vue";
-import SignUp from "./components/SignUp.vue";
-import { createRouter, createWebHistory } from "vue-router";
-import LogIn from "./components/LogIn.vue";
-
+import HomePage from "./components/Home.vue"
+import SignUp from "./components/SignUp.vue"
+import { createRouter, createWebHistory } from "vue-router"
+import LogIn from "./components/LogIn.vue"
+import UpdatePage from "./components/Update.vue"
+import AddPage from "./components/Add.vue";
 const routes=[
     {path:'/', name:'HomePage', component:HomePage },
     {path:'/sign-up', name:'SignUp', component: SignUp },
-    {path:'/log-in', name:'LogIn',component: LogIn }
-
+    {path:'/log-in', name:'LogIn',component: LogIn },
+    {path:'/update', name:'UpdatePage',component: UpdatePage },
+    {path:'/add', name:'AddPage',component: AddPage },
 ];
 
 const router=createRouter({
