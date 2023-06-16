fc005f1d48b82c02522607df74c93f911981f664
Brian Rono <chikoe26@gmail.com>
2023-06-16 15:23:33 +0300

    m

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,39 @@
 <template>
     <Header />
-    <h1>Hello User, Welcome To Home Page</h1>
+    <h1>Hello k {{user}}, Welcome To Home Page</h1>
 </template>
 
 <script>
 import Header from './Header.vue'
+import axios from "axios"
 export default {
     name:'HomePage',
+    data(){
+        return {
+            name:''
+        }
+    },
     components:{
         Header
+    },
+    methods: {
+        fetchData() {
+        axios.get(`http://localhost:3000/users`)
+        .then(response => {
+          this.name = response.user;
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+    }
     },
     mounted()
     {
         let user= localStorage.getItem('user-info');
+        this.name =  axios.get(`http://localhost:3000/users`)
+        .then(response => {
+          this.name = response.user;
+        });
         if(user)
         {
             this.$router.push({name:'SignUp'})
